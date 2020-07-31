2f455344521cb7751311006c02c97c1284d3b307
mmomin11 <mubin.momin@nxp.com>
2020-07-30 21:01:17 -0500

    updated the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import {RiMenu4Line} from 'react-icons/ri'
+import {FiSettings} from 'react-icons/fi'
+import Teams from './TeamDropdown'
+
+
 
 function Navbar(props) {
   return (
     <div className="navbar">
-      <h1>Dropdowns</h1>
+      <div className="flex-row">
+      <Teams title="Team" item1="Team1" item2="Team2"/>
+      <Teams title="User" item1="User1" item2="User2"/>
+      </div>
       <div className="flex-row">
         <RiMenu4Line color="lightblue" style={{fontSize: '20px', margin: '1px'}}/>
         <h1 className="race-font">Autoban</h1>
       </div>
-      <h1
+
+      <FiSettings style={{margin: "5px"}}
         onClick={() => {
           props.setdrawer({
             ...props.drawer,
             open: !props.drawer.open,
           });
         }}
-      >
-        Settings
-      </h1>
+      />
+
+      
     </div>
   );
 }
