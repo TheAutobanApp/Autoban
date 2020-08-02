7e30717cd3699e06be3d17aa4fd4f8b464c711e6
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-02 11:31:50 -0500

    Built get, create, and update routes for user

--- controllers/apiRoutes/user.js
+++ controllers/apiRoutes/user.js
@@ -34,9 +34,112 @@ router.post('/', function (req, res) {
   }
 });
 
-
+// update user info
 router.put('/', function (req, res) {
-  //do things here for other routes
+  // update username
+  if (req.query.un !== req.query.newun) {
+    db.User.findOne({
+      where: { username: req.query.un },
+    }).then((user) => {
+      if (user) {
+        user
+          .update({ username: req.query.newun })
+          .then(() => {
+            res.json(user);
+          })
+          .catch((err) => {
+            res.status(401).json(err);
+          });
+      }
+    }).catch((err) => {
+      res.status(401).json(err);
+    });
+  }
+  // update email
+  if (req.query.email !== req.query.newemail) {
+    db.User.findOne({
+      where: { email: req.query.email },
+    }).then((user) => {
+      if (user) {
+        user
+          .update({ email: req.query.newemail })
+          .then(() => {
+            res.json(user);
+          })
+          .catch((err) => {
+            res.status(401).json(err);
+          });
+      }
+    }).catch((err) => {
+      res.status(401).json(err);
+    });
+  }
+  // update password
+  if (req.query.pw !== req.query.newpw) {
+    db.User.findOne({
+      where: { password: req.query.pw, id_user: req.query.uid },
+    }).then((user) => {
+      if (user) {
+        user
+          .update({ password: req.query.newpw })
+          .then(() => {
+            res.json(user);
+          })
+          .catch((err) => {
+            res.status(401).json(err);
+          });
+      }
+    }).catch((err) => {
+      res.status(401).json(err);
+    });
+  }
+});
+
+// update name
+router.put('/:name', function (req, res) {
+    if (req.param.name) {
+      // udpate first name
+      if (req.query.fn !== req.query.newfn) {
+        db.User.findOne({
+          where: { first_name: req.query.fn, id_user: req.query.uid },
+        }).then((user) => {
+          if (user) {
+            user
+              .update({ first_name: req.query.newfn })
+              .then((user) => {
+                // if there is a new last name, don't respond
+                if (!req.query.newln) {
+                  res.json(user);
+                }
+              })
+              .catch((err) => {
+                res.status(401).json(err);
+              });
+          }
+        }).catch((err) => {
+          res.status(401).json(err);
+        });
+      }
+      // update last name
+      if (req.query.ln !== req.query.newln) {
+        db.User.findOne({
+          where: { last_name: req.query.ln, id_user: req.query.uid },
+        }).then((user) => {
+          if (user) {
+            user
+              .update({ last_name: req.query.newln })
+              .then((user) => {
+                res.json(user);
+              })
+              .catch((err) => {
+                res.status(401).json(err);
+              });
+          }
+        }).catch((err) => {
+          res.status(401).json(err);
+        });
+    }
+  }
 });
 
 router.delete('/', function (req, res) {
