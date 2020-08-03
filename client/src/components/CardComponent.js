9ac2efcf1c3fe89abe0c6085f4fb2957dfc62205
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-03 16:36:52 -0500

    Added dropdown and positioned using reactDOM offsets. Conditionally rendering labels and options from dummy data

--- client/src/components/CardComponent.js
+++ client/src/components/CardComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { Card, Button, Overlay, Tooltip } from 'react-bootstrap';
-import { Icon, Label } from 'semantic-ui-react';
+import { Icon, Label, Dropdown, Input } from 'semantic-ui-react';
 // import { IoIosArrowDropright } from 'react-icons/io';
 // import { FaEllipsisV } from 'react-icons/fa';
 import DropMenu from './DropMenu';
@@ -8,25 +9,44 @@ import DropMenu from './DropMenu';
 function CardComponent(props) {
   const [show, setShow] = useState(false);
   const target = useRef(null);
+  const [menu, setMenu] = useState({ offsetTop: 0, offsetLeft: 0 });
 
-  const dummy = [
+  const [dummy, setDummy] = useState([
     {
       color: 'green',
       text: 'Back End',
+      added: false,
     },
     {
       color: 'teal',
       text: 'Front End',
+      added: false,
     },
     {
       color: 'yellow',
       text: 'MVP',
+      added: false,
     },
     {
       color: 'red',
       text: 'Bug',
+      added: false,
     },
-  ];
+  ]);
+
+  const handleLabelDelete = (e) => {
+    let i = e.target.parentElement.id;
+    const dummyCopy = Array.from(dummy);
+    dummyCopy[i].added = false;
+    setDummy(dummyCopy);
+  };
+
+  const handleAddLabel = (i) => {
+    const dummyCopy = Array.from(dummy);
+    dummyCopy[i].added = true;
+    setDummy(dummyCopy);
+  };
+
   return (
     <Card className="card">
       <Card.Body style={{ display: 'inline-block' }}>
@@ -51,23 +71,91 @@ function CardComponent(props) {
         }}
       >
         {dummy.map((item, i) => {
-          return (
-            <Label size="mini" color={item.color} circular>
-              {item.text}
-              <Icon name="delete" />
-            </Label>
-          );
+          if (item.added) {
+            return (
+              <Label
+                size="mini"
+                color={item.color}
+                circular
+                id={i}
+                key={i}
+              >
+                {item.text}
+                <Icon name="delete" onClick={handleLabelDelete} />
+              </Label>
+            );
+          }
         })}
-        <Label
-          size="mini"
-          color="blue"
-          circular
-          basic
-          className="clickable"
+
+        <Dropdown
+          ref={target}
+          trigger={
+            <Label
+              size="mini"
+              color="blue"
+              circular
+              basic
+              className="clickable"
+            >
+              <Icon name="add" />
+              Label
+            </Label>
+          }
+          labeled
+          id={`add${props.id}`}
+          onClick={(e) => {
+            const targ = ReactDOM.findDOMNode(target.current);
+            setMenu({
+              offsetTop:
+                targ.offsetTop +
+                targ.parentElement.offsetTop +
+                targ.parentElement.parentElement.offsetTop,
+              offsetLeft:
+                targ.offsetLeft +
+                targ.parentElement.parentElement.offsetLeft,
+            });
+            // console.log(offsetTop)
+          }}
         >
-          <Icon name="add" />
-          Label
-        </Label>
+          <Dropdown.Menu
+            style={{
+              position: 'fixed',
+              top: menu.offsetTop,
+              left: menu.offsetLeft,
+              minWidth: 'fit-content',
+              zIndex: 1000,
+            }}
+          >
+            <Input
+              size="mini"
+              icon="search"
+              iconPosition="left"
+              className="search"
+            />
+            <Dropdown.Divider />
+            <Dropdown.Menu scrolling>
+              {dummy.map((option, i) => {
+                if (!dummy[i].added) {
+                  return (
+                    <Dropdown.Item
+                      key={option.text}
+                      text={option.text}
+                      value={option.text}
+                      onClick={() =>
+                        handleAddLabel(i)
+                      }
+                      label={{
+                        color: option.color,
+                        empty: true,
+                        circular: true,
+                      }}
+                    />
+                  );
+                }
+              })}
+            </Dropdown.Menu>
+          </Dropdown.Menu>
+        </Dropdown>
 
         {/* <Button
           size="sm"
