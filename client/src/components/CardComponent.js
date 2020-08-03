d3e58f5dff5548dbdbe36e13f630cca7a964fc34
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-03 14:32:42 -0500

    Created basic label layout using semanticUI lablels. Using dummy data to render labels dynamically

--- client/src/components/CardComponent.js
+++ client/src/components/CardComponent.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Card, Button, Overlay, Tooltip } from 'react-bootstrap';
+import { Icon, Label } from 'semantic-ui-react';
 // import { IoIosArrowDropright } from 'react-icons/io';
 // import { FaEllipsisV } from 'react-icons/fa';
 import DropMenu from './DropMenu';
@@ -7,6 +8,25 @@ import DropMenu from './DropMenu';
 function CardComponent(props) {
   const [show, setShow] = useState(false);
   const target = useRef(null);
+
+  const dummy = [
+    {
+      color: 'green',
+      text: 'Back End',
+    },
+    {
+      color: 'teal',
+      text: 'Front End',
+    },
+    {
+      color: 'yellow',
+      text: 'MVP',
+    },
+    {
+      color: 'red',
+      text: 'Bug',
+    },
+  ];
   return (
     <Card className="card">
       <Card.Body style={{ display: 'inline-block' }}>
@@ -24,9 +44,32 @@ function CardComponent(props) {
       </Card.Body>
       <Card.Footer
         className="flex-row card-footer"
-        style={{ alignItems: 'center' }}
+        style={{
+          alignItems: 'center',
+          height: 'fit-content',
+          flexWrap: 'wrap',
+        }}
       >
-        <Button
+        {dummy.map((item, i) => {
+          return (
+            <Label size="mini" color={item.color} circular>
+              {item.text}
+              <Icon name="delete" />
+            </Label>
+          );
+        })}
+        <Label
+          size="mini"
+          color="blue"
+          circular
+          basic
+          className="clickable"
+        >
+          <Icon name="add" />
+          Label
+        </Label>
+
+        {/* <Button
           size="sm"
           variant="outline-dark"
           ref={target}
@@ -46,7 +89,7 @@ function CardComponent(props) {
               P1,{' '}
             </Tooltip>
           )}
-        </Overlay>
+        </Overlay> */}
       </Card.Footer>
     </Card>
   );
