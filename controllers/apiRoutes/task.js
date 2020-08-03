b0d8f19136fc77e99f74f493d4d5b87645e3c25a
andrewvensoniv <68923865+andrewvensoniv@users.noreply.github.com>
2020-08-02 19:51:53 -0500

    pass json

--- controllers/apiRoutes/task.js
+++ controllers/apiRoutes/task.js
@@ -26,7 +26,7 @@ router.post('/create/:proj_id', function (req, res) {
     id_label3: task.id_label3,
     complete: task.complete,
   }).then((result) => {
-    res.redirect(`/api/task/get/all/${task.proj_id}`);
+    res.json(result);
   });
 });
 
