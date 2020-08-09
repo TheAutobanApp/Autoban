263364b8deeb1ca3319d1abc92b85ba0fe194082
Tanner Griffin <tannermgriffin@gmail.com>
2020-08-09 17:43:20 -0500

    Added logo to project cards

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Card, Label } from 'semantic-ui-react';
+import { RiMenu4Line } from 'react-icons/ri';
 import { AutoContext } from '../AutoContext';
 
 export default function ProjectCard(props) {
@@ -9,12 +10,13 @@ export default function ProjectCard(props) {
     context[11]({ type: 'project', project: props.id });
     context[9]({ ...context[8], team: null });
   };
-  
+
   return (
     <Label
       className="clickable"
       color={props.color}
       style={{
+        position: 'relative',
         height: '100px',
         width: '160px',
         borderRadius: '5px',
@@ -22,10 +24,19 @@ export default function ProjectCard(props) {
         margin: '4px 4px 4px',
         fontWeight: 'bold',
         color: 'white',
-        padding: 0
+        padding: 0,
       }}
       onClick={handleProjectSelect}
     >
+      <div className="flex-row" style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-60%, -50%)', padding: '4px'}}>
+          <RiMenu4Line
+            // color="lightblue"
+            style={{ fontSize: '20px', margin: '1px', color: 'rgba(255, 255, 255, .2)' }}
+          />
+          <h1 className="race-font" style={{ color: 'rgba(255, 255, 255, .2)'}}>
+            A
+          </h1>
+        </div>
       <div
         style={{
           display: 'flex',
@@ -38,6 +49,7 @@ export default function ProjectCard(props) {
           background: `linear-gradient(to bottom, transparent, rgba(10, 10, 10, .4))`,
         }}
       >
+        
         {props.title}
       </div>
     </Label>
