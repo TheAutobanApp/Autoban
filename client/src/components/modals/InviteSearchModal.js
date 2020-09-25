198fda80068b5543b99c3e8585b9f3b044537e9f
Andrew E. Venson <Andrew.Venson@integralcare.org>
2020-09-24 21:45:25 -0500

    added semantic search input, set selected user in state

--- client/src/components/modals/InviteSearchModal.js
+++ client/src/components/modals/InviteSearchModal.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Rodal from 'rodal';
 import { AutoContext } from '../../AutoContext';
 import 'rodal/lib/rodal.css';
-import { List, Button, Icon, Input } from 'semantic-ui-react';
+import { List, Button, Icon, Image, Search } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default function InviteSearchModal(props) {
@@ -14,9 +14,27 @@ export default function InviteSearchModal(props) {
     backgroundColor: 'whitesmoke',
   };
 
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState('');
+  const [selectedUser, setSelectedUser] = useState({
+    selected: false,
+    username: '',
+    name: '',
+  });
   // hide modal, reset state and modal context
   const hideModal = () => {
     context[5]({ ...context[4], showSearch: false });
+  };
+
+  const searchUser = (searchTxt) => {
+    if (searchTxt.length > 1) {
+      axios
+        .get(`/api/user/search/?search=${searchTxt}`)
+        .then((response) => {
+          console.log(response.data);
+          setUsers(response.data);
+        });
+    }
   };
 
   return (
@@ -26,7 +44,45 @@ export default function InviteSearchModal(props) {
       customStyles={modalStyle}
     >
       <div className="invite-modal">
-        <Input placholder="Search" />
+        {selectedUser.selected ? (
+          <List>
+            <List.Item>
+              <Image
+                avatar
+                src="https://react.semantic-ui.com/images/avatar/small/rachel.png"
+              />
+              <List.Content>
+                <List.Header as="a">{selectedUser.name}</List.Header>
+                <List.Description>
+                  {selectedUser.username}
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          </List>
+        ) : (
+          <Search
+            placeholder="Search"
+            value={search}
+            onResultSelect={(e, data) =>
+              setSelectedUser({
+                ...selectedUser,
+                selected: true,
+                username: data.result.description,
+                name: data.result.title,
+              })
+            }
+            results={users.map((user) => {
+              return {
+                title: `${user.first_name} ${user.last_name}`,
+                description: user.username,
+              };
+            })}
+            onSearchChange={(e) => {
+              setSearch(e.target.value);
+              searchUser(e.target.value);
+            }}
+          />
+        )}
         <button
           className="saveButton"
           onClick={() => {
