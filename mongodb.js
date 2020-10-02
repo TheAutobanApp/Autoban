31dcb9cae9ac96dd803381959625991b3d18adbe
Tanner Griffin <tannermgriffin@gmail.com>
2020-10-02 14:36:59 -0500

    Created local mongodb config, and conditional config based on node_env

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,23 @@
-require('dotenv').config()
 const mongoose = require('mongoose');
-const connection = `mongodb+srv://autoban:${process.env.MONGODB}@autoban.ulrka.mongodb.net/autoban?retryWrites=true&w=majority`;
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  const connection = `mongodb+srv://autoban:${process.env.MONGODB}@autoban.ulrka.mongodb.net/autoban?retryWrites=true&w=majority`;
+  mongoose
+    .connect(connection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('Database Connected Successfully'))
+    .catch((err) => console.log(err));
+} else {
+  var mongoDB = 'mongodb://localhost:27017/autoban_data';
+  mongoose
+    .connect(mongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('Local Database Connected Successfully'))
+    .catch((err) => console.log(err));
+}
