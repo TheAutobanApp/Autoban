3881973c2b1c8489b8d691d0e68a824e2cdad7d3
Tanner Griffin <tannermgriffin@gmail.com>
2020-10-01 19:22:10 -0500

    Added mongodb connect file

---
+++ mongodb.js
@@ -0,0 +1,5 @@
+// const mongoose = require('mongoose');
+// const connection = `mongodb+srv://autoban:${process.env.mongodb}@autoban.ulrka.mongodb.net/Autoban?retryWrites=true&w=majority`;
+// mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+//     .then(() => console.log("Database Connected Successfully"))
+//     .catch(err => console.log(err));
